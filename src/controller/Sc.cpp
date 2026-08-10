97c770af7fd70dfdd4c0a920f324774427709fec
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-10 18:16:24 +0530

    rfr-1.1

--- src/controller/Sc.cpp
+++ src/controller/Sc.cpp
@@ -1,17 +1,27 @@
-#include "SimController.h"
+#include "Sc.h"
 
 SimController::SimController(WaveformModel *model, QObject *parent)
-    : QObject(parent), m_model(model) {
+    : QObject{parent},
+      m_model{model},
+      m_timer{},
+      m_elapsed{}
+{
     connect(&m_timer, &QTimer::timeout, this, &SimController::onTick);
-    m_timer.setInterval(16); 
+    m_timer.setInterval(16);
 }
 
-void SimController::start() {
+void SimController::start()
+{
     m_elapsed.start();
     m_timer.start();
 }
 
-void SimController::onTick() {
-    double dt = m_elapsed.restart() / 1000.0;
+void SimController::onTick()
+{
+    if (!m_model) {
+        return;
+    }
+
+    double dt{static_cast<double>(m_elapsed.restart()) / 1000.0};
     m_model->renderFrame(dt);
 }
